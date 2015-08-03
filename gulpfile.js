e7dc3f31a2a62f71bec4cc4cdb308ac54ad7bd5a
jjballano <jjballano@gmail.com>
2015-08-03 14:20:28 +0100

    Gulp-react added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    react = require('gulp-react');
+
 
 gulp.task('default', function(){
-
+  return gulp.src('template.jsx')
+             .pipe(react())
+             .pipe(gulp.dest('dist'));
 });
